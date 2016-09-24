b77310e6787535e1a265d6020f0060b75f3d373d
Kirsten Meeker <kmeeker@cs.ucsb.edu>
2016-09-24 10:43:13 -0700

    hourly collection & conversion working

--- noir.py
+++ noir.py
@@ -3,19 +3,35 @@
 import picamera
 from time import sleep
 import subprocess
+import time 
 
 # configuration
 
 camera = picamera.PiCamera()
-delay = 3600
+delay = 3599
 from subprocess import call
 
 for i in range(1,24):
-    filename = 'image'+str(i)+'.jpg'
+    timestr = time.strftime("%Y%m%d-%H%M")
+    filename = 'image' + timestr + '.jpg'
     camera.capture(filename)
+
     cmd_str='/usr/local/bin/infrapix_single -i '+filename+' --show_histogram -o ndvi_'+filename
     print(cmd_str)
-    return_code = subprocess.call(["/usr/local/bin/infrapix_single","-i",filename,"--show_histogram","-o","ndvi_"+filename])
+    return_code = subprocess.call(cmd_str, shell=True)
+
+    cmd_str='/usr/local/bin/infrapix_single -i '+filename+' --vmin -1.0 --vmax 1.0 --show_histogram -o ndvi_'+filename
+    print(cmd_str)
+    return_code = subprocess.call(cmd_str, shell=True)
+
+    cmd_str='/usr/local/bin/infrapix_single -i '+filename+' --vmin -1.0 --vmax 1.0 -o ndvi_nh_'+filename
+    print(cmd_str)
+    return_code = subprocess.call(cmd_str, shell=True)
+
+    cmd_str='~/NoIR $ ~/Dropbox-Uploader/dropbox_uploader.sh upload *.jpg *.jpg'
+    print(cmd_str)
+    return_code = subprocess.call(cmd_str, shell=True)
+
     sleep(delay)
 
 
