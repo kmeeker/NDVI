5daa856cb6020f2ad0364089714ff6af0f2f7adf
Kirsten Meeker <kmeeker@cs.ucsb.edu>
2016-10-01 15:00:52 -0700

    noir_single update at SD maker faire

--- noir_single.py
+++ noir_single.py
@@ -12,7 +12,7 @@ camera = picamera.PiCamera()
 timestr = time.strftime("%Y%m%d-%H%M")
 filename = 'image_single_' + timestr + '.jpg'
 camera.capture(filename)
-cmd_str='/usr/local/bin/infrapix_single -i '+filename+' --show_histogram -o ndvi_'+filename
+cmd_str='/usr/local/bin/infrapix_single -i '+filename+' --vmin -1.0 --vmax 1.0 --show_histogram -o ndvi_'+filename
 print(cmd_str)
 return_code = subprocess.call(["/usr/local/bin/infrapix_single","-i",filename,"--show_histogram","-o","ndvi_"+filename])
 
